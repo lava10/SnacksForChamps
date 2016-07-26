4d7a7be4e9640d84576adf528cd9fe0ca572f33a
Valiente, Laura <laura.valiente@nordstrom.com>
2016-07-26 16:06:10 -0700

    menu options started

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MenuApplication
+{
+    class Food
+    {
+        string eggs = "Eggscellent Benedict";
+        string bacon = "All the Bacon Omelette";
+        string pancakes = "Ultra Fluffy Pancakes";
+        string waffles = "Stacked Waffles";
+        string potatoes = "Rad Hash Browns";
+        string fruit = "Fruit Salad";
+        string regularBurger = "Super Stacked Burger";
+        string doubleBurger = "Double Double Stacked Burger";
+        string cheeseBurger = "Extra Cheesy CheeseBurger";
+        string veggieBurger = "Weird Garden Burger";
+        string baconBurger = "Bacon Bleu Cheese Burger";
+        string fries = "Medium Fries";
+        string extraSides = "Cheesy Fries and Jalepenos";
+        string onionRings = "Boozey Beer Battered Onion Rings";
+        string jalepenoPoppers = "Spicy Jalepeno Poppers";
+        string tacos = "Street Style Tacos";
+        string sliders = "Brazillian Steakhouse Sliders";
+        string pizza = "Pizza is Life Pizza";
+        string nachos = "YO! Nachos";
+    }
+
+class Drinks
+{
+    string regularSoda = "Coca Cola";
+    string otherSoda = "Sprite";
+    string dietSoda = "Diet Coca Cola";
+    string otherDrinks = "Water";
+    string vodka = "Vodka Cranberry";
+    string vodkaMix = "Bloody Mary";
+    string whiskey = "Old Fashioned";
+    string gin = "Martini";
+    string beer = "Mac & Jacks";
+    string wine = "Washington State Merlot";
+}
+
+class Menu
+{
+    static void MainInfo()
+    {
+
+    }
+    static void BreakfastOptions()
+    {
+    }
+
+    static void LunchOptions()
+    {
+    }
+
+    static void HappyHourOptions()
+    {
+    }
+    static void Specials()
+    {
+
+    }
+}
+}
---
+++ csharp.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApplication
+{
+    class Hello
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("This is C#");
+            Console.ReadLine();
+        }
+    }
+}
